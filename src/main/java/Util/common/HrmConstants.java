ab41704c24fcaf271b578ed60f4393e8de3d206f
guozifan123 <826148267@qq.com>
2018-04-27 12:42:17 -0300

    some of sql constants's declared

---
+++ src/main/java/Util/common/HrmConstants.java
@@ -0,0 +1,16 @@
+package Util.common;
+
+public class HrmConstants {
+    public static final String USERTABLE = "user_inf";
+    public static final String DEPTTABLE = "dept_inf";
+    public static final String JOBTABLE = "job_inf";
+    public static final String EMPLOYEETABLE = "employee_inf";
+    public static final String NOTICETABLE = "notice_inf";
+    public static final String BOCUMENTTABLE = "document_inf";
+    //登录
+    public static final String LOGIN = "loginForm";
+    //用户的session对象
+    public static final String USER_SESSION = "user_session";
+    //默认4页的数据
+    public static int PAGE_DEFAULT_SIZE = 4;
+}
